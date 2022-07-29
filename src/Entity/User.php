c1e2c60a708af1b50f4342198c6aab63299eab29
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-07-29 14:54:56 +0200

    One to many relationship between User and Customer

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -40,6 +42,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255)]
     private string $company;
 
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Customer::class, orphanRemoval: true)]
+    private Collection $customer;
+
     public function __construct(string $email, string $password, string $firstname, string $lastname, string $company)
     {
         $this->email = $email;
@@ -47,6 +52,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->firstname = $firstname;
         $this->lastname = $lastname;
         $this->company = $company;
+        $this->customer = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -125,6 +131,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getCompany(): ?string
     {
         return $this->company;
+    }
+
+    /**
+     * @return Collection<int, Customer>
+     */
+    public function getCustomer(): Collection
+    {
+        return $this->customer;
+    }
+
+    public function addCustomer(Customer $customer): self
+    {
+        if (!$this->customer->contains($customer)) {
+            $this->customer->add($customer);
+            $customer->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCustomer(Customer $customer): self
+    {
+        if ($this->customer->removeElement($customer)) {
+            // set the owning side to null (unless already changed)
+            if ($customer->getUser() === $this) {
+                $customer->setUser(null);
+            }
+        }
+
+        return $this;
     }
 
 }
