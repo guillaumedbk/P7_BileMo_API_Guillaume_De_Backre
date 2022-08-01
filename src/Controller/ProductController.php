fea8a3ca138b88cab834805506dfe591ef662c8e
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-08-01 11:32:46 +0200

    method to retrieve all products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,19 +3,20 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/products', name: 'app_product', methods: ['GET'])]
-    public function getAllProducts(ProductRepository $productRepository): JsonResponse
+    #[Route('/products', name: 'app_products', methods: ['GET'])]
+    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
         $products = $productRepository->findAll();
+        $jsonProducts = $serializer->serialize($products, 'json');
 
-        return new JsonResponse([
-            'products' => $products
-        ]);
+        return new JsonResponse($jsonProducts, Response::HTTP_OK, [], true);
     }
 }
