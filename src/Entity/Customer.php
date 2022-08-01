a97ebc06898f8009bff95fac4fe1594c01a1e2e4
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-08-01 15:34:06 +0200

    Route to view the details of a registered customer linked to a user
    Add unique identifier and use it to retrieve user

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CustomerRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: CustomerRepository::class)]
 class Customer
@@ -28,13 +29,17 @@ class Customer
     #[ORM\ManyToOne(inversedBy: 'customer')]
     #[ORM\JoinColumn(nullable: false)]
     private User $user;
-    
+
+    #[ORM\Column(length: 255)]
+    private string $identifier;
+
     public function __construct(string $firstname, string $lastname, string $email, string $password)
     {
         $this->firstname = $firstname;
         $this->lastname = $lastname;
         $this->email = $email;
         $this->password = $password;
+        $this->identifier = $firstname . '-' . $lastname . '-' . Uuid::v1();
     }
 
     public function getId(): ?int
@@ -73,4 +78,10 @@ class Customer
 
         return $this;
     }
+
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
 }
