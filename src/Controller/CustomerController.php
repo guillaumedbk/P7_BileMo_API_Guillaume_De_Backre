fa431f75b8ce54a183ffd7df20efc4768f9f68fa
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-08-02 15:39:54 +0200

    precise methods and retrieve customer on the basis of their identifier

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Customer;
+use App\Entity\User;
 use App\Repository\CustomerRepository;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CustomerController extends AbstractController
 {
-    #[Route('{id}/customers', name: 'app_user_customers')]
+    #[Route('{id}/customers', name: 'app_user_customers', methods:['GET'])]
     public function getAllCustomers(User $user, CustomerRepository $customerRepository, SerializerInterface $serializer): JsonResponse
     {
         $customers = $customerRepository->findBy(['user' => $user]);
@@ -20,15 +22,12 @@ class CustomerController extends AbstractController
         return new JsonResponse($jsonCustomers, Response::HTTP_OK, [], true);
     }
 
-    #[Route('/customer/{identifier}', name: 'app_customer_detail')]
-    public function getCustomerDetail(string $identifier, CustomerRepository $customerRepository, SerializerInterface $serializer): JsonResponse
+    #[Route('/customer/{identifier}', name: 'app_customer_detail', methods: ['GET'])]
+    public function getCustomerDetail(Customer $customer, CustomerRepository $customerRepository, SerializerInterface $serializer): JsonResponse
     {
-        $customer = $customerRepository->findBy(['identifier' => $identifier]);
-        if ($customer) {
-            $jsonCustomers = $serializer->serialize($customer, 'json');
-            return new JsonResponse($jsonCustomers, Response::HTTP_OK, [], true);
-        }
-        return new JsonResponse(null, Response::HTTP_NOT_FOUND, []);
+        $customer = $customerRepository->findBy(['identifier' => $customer->getIdentifier()]);
+        $jsonCustomers = $serializer->serialize($customer, 'json');
 
+        return new JsonResponse($jsonCustomers, Response::HTTP_OK, [], true);
     }
 }
